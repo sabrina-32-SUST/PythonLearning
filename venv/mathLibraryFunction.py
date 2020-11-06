81d11eb8493043e08e69722c5323a36268c8af0b
Sabrina <sanutaayvi@gmail.com>
2020-11-06 17:44:32 +0600

    math library

---
+++ venv/mathLibraryFunction.py
@@ -0,0 +1,12 @@
+# Math  Library  Function
+from math import *
+
+result = max(20,40)
+print(result)
+print(abs(-54))
+print(pow(2,3))
+print(sqrt(25))
+print(round(3,2))
+print(min(4,7))
+print(floor(2.5))
+print(ceil(6.7))
