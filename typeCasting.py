5838716e69a93d410a0d7a1fbba996bf40a52ed9
Sabrina <sanutaayvi@gmail.com>
2020-11-06 10:25:04 +0600

    Type Casting

---
+++ typeCasting.py
@@ -0,0 +1,20 @@
+#Type  Casting
+##############################
+
+num1 = int(input("Enter first  number : "))
+num2 = int(input("Enter Second  number : "))
+
+result  =  num1 +  num2
+print("The  result  is : ",result)
+
+result  =  num1 -  num2
+print("The  result  is : ",result)
+
+result  =  num1 *  num2
+print("The  result  is : ",result)
+
+result  =  num1 /  num2
+print("The  result  is : ",result)
+
+result  =  num1 %  num2
+print("The  result  is : ",result)
