773bbd7373c10aa60d54bc259723ce3c1d1a13f4
Sabrina <sanutaayvi@gmail.com>
2020-11-06 10:13:38 +0600

    first commit

---
+++ gettingUserInput.py
@@ -0,0 +1,11 @@
+#Getting  User  Input
+###############################
+name = input("Enter your name ")
+age = input("enter your age ")
+cgpa = input("Enter your  CGPA ")
+
+print(" Student  Information")
+print("----------------------")
+print("Name :"+ name)
+print("Age :" + age)
+print("CGPA :" +cgpa)
