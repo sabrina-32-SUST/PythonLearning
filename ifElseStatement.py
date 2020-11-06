e41f08a9b1eca8c321ae0a7aa86759f826b758bb
Sabrina <sanutaayvi@gmail.com>
2020-11-06 18:08:50 +0600

    IfELse Condition

---
+++ ifElseStatement.py
@@ -0,0 +1,15 @@
+#pass/fail
+mark = int(input("Enter your  marks :"))
+if(mark>=33):
+    print("Pass")
+
+else:print("Fail")
+
+#Odd/even
+
+num= int(input("Enter The  Number :"))
+
+if(num%2==0):
+    print("Even")
+else:print("Odd")
+
