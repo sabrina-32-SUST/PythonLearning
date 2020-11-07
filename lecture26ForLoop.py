651fc34cf2e15f16dc5afe19946bd948cba9cc80
Sabrina <sanutaayvi@gmail.com>
2020-11-07 21:18:25 +0600

    for loop

---
+++ lecture26ForLoop.py
@@ -0,0 +1,18 @@
+num = [10,20,40,50,60]
+print(num)
+
+count = 0
+while count<=4:
+    print(num[count])
+    count+=1
+
+n = len(num)
+print(n)
+count = 0
+
+while count<=n:
+    print(num[count])
+    count+=1
+
+for x in num:
+    print(x)
