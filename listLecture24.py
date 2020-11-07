3a2c24f74304df9226c54c3d7377dab2f72293bc
Sabrina <sanutaayvi@gmail.com>
2020-11-07 09:47:14 +0600

    List

---
+++ listLecture24.py
@@ -0,0 +1,14 @@
+subjects = ["C", "C++", "Java","JavaScript","Python"]
+
+print(subjects)
+
+print(subjects[0])
+print(subjects[2:])
+print(subjects[-1])
+print("Python" in subjects)
+print("Swift" in subjects)
+print("Swift" not in subjects)
+print("Python" not in subjects)
+print(subjects+["Swift", 233])
+print(subjects*3)
+
