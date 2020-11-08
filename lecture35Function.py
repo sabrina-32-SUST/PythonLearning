7f01129eaa542a8251e4a186125406e741d35bc1
Sabrina <sanutaayvi@gmail.com>
2020-11-08 19:50:51 +0600

    FUnction

---
+++ lecture35Function.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    sum= a+b
+    print(sum)
+
+add(10,4)
+
+def sub(a,b):
+    sum= a-b
+    print(sum)
+sub(6,3)
+def mul(a,b):
+    sum= a*b
+    print(sum)
+mul(5,2)
+
+def div(a,b):
+    sum= a/b
+    print(sum)
+div(9,3)
