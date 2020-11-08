871eb7c4e8d0438591ecce6c2456ea2740c8a092
Sabrina <sanutaayvi@gmail.com>
2020-11-08 20:01:26 +0600

    returning function

--- lecture35Function.py
+++ lecture35Function.py
@@ -1,19 +1,19 @@
 def add(a,b):
-    sum= a+b
-    print(sum)
+    result= a+b
+    print(result)
 
 add(10,4)
 
 def sub(a,b):
-    sum= a-b
-    print(sum)
+    result= a-b
+    print(result)
 sub(6,3)
 def mul(a,b):
-    sum= a*b
-    print(sum)
+    result= a*b
+    print(result)
 mul(5,2)
 
 def div(a,b):
-    sum= a/b
-    print(sum)
+    result= a/b
+    print(result)
 div(9,3)
---
+++ lecture36ReturningValueFromFunction.py
@@ -0,0 +1,14 @@
+def  add(a,b):
+    sum = a+b
+    return sum
+result = add(20,40)
+print("result", result)
+
+def  large(a,b):
+    if a>b:
+        return a
+    else:
+        return b
+result = large(50,40)
+print("result", result)
+
