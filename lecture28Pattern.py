de0a2c41447030fff49bd8fe584c00ecc6267f5f
Sabrina <sanutaayvi@gmail.com>
2020-11-08 11:26:58 +0600

    Pattern

---
+++ lecture28Pattern.py
@@ -0,0 +1,13 @@
+print("Pattern: 1")
+
+n= int(input("enter number: "))
+for  x  in range(n+1):
+    print(x*"*")
+
+print("Pattern: 2")
+n= int(input("enter number: "))
+
+for  x  in range(1,n+1,2):
+    print(x*"*")
+
+ 
