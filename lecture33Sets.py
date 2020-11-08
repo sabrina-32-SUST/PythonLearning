409696fb4c0007f180517805df79c0e4d8e71227
Sabrina <sanutaayvi@gmail.com>
2020-11-08 19:07:27 +0600

    Sets

---
+++ lecture33Sets.py
@@ -0,0 +1,15 @@
+num1 = {1,2,3,4,5}
+print(num1)
+num2 = set([4,5,4,3,7,6])
+print(num2)
+num1.add(6)
+print(num1)
+num2.remove(7)
+print(num2)
+print(4  in  num2)
+print(4  not in num2)
+
+print(num2| num1)
+print(num2 & num1)
+print(num2 - num1)
+
