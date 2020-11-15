c2fddc9daa0286ebe1a1aa76fda5a47fb628d8c9
Sabrina <sanutaayvi@gmail.com>
2020-11-15 11:48:14 +0600

    Constructor

---
+++ lecture49Constructor.py
@@ -0,0 +1,15 @@
+class  Student:
+    roll = ""
+    gpa = ""
+
+    def __init__(self, roll, gpa):
+      self.roll = roll
+      self.gpa = gpa
+    def  display(self):
+        print(f"Roll: {self.roll}, GPA: {self.gpa}")
+
+
+
+
+rahim = Student(101, 3)
+rahim.display()
