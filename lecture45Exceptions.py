84653fc1067888f2ca56475fbcd565e8031b43e3
Sabrina <sanutaayvi@gmail.com>
2020-11-15 10:33:10 +0600

    Exception Handling

--- lecture45Exceptions.py
+++ lecture45Exceptions.py
@@ -1,16 +1,29 @@
+# try:
+#
+#     num1 = input("enter number1: ")
+#     num2 = input("enter number2: ")
+#     result = num2/num1
+#     print(result)
+#
+#
+# except(ZeroDivisionError, ValueError) :
+#     print("You  have  entered  incorrect  input")
+#
+# finally:
+#     print("Successful")
+
+
+
+def  voter(age):
+    if age<18:
+        raise   ValueError("Invalid  Voter")
+    return "You  are  allow  to  Vote"
+
+print(voter(19))
+
 try:
-    list = [20, 0, 40, 50]
-    result = list[0] / list[5]
-    print(result)
-    print("done")
-
-
-
-except ZeroDivisionError:
-    print("Dividing by  zero  is  not  possible")
- 
-finally:
-    print("Successful")
-
+    print(voter(17))
+except ValueError as  error:
+    print(e)
 
 
